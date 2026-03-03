58f098b97fd96452ac8e1a307ab4e05449f5f01c
Partha Modak <44617799+parthamodak@users.noreply.github.com>
2026-03-03 21:34:29 +0000

    Improved code comments and documentation

--- Recursion.py
+++ Recursion.py
@@ -9,9 +9,12 @@ def open_doll(size):
 
     open_doll(size - 1)                             # (6)
 
-    print("Closing doll", size)                     # (7)
+    print("Closing doll", size)                     
 
 
 
 size = int(input("Enter the number of dolls: "))
-open_doll(size)                                          # (8)
+open_doll(size)                                          
+
+# here is a thing when this code exicute it allocate the same amount of
+#  the memory what it need to open then it for the loasing time it require same like open is 10 so ending will be 10 total 20 memory allocated
