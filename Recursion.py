79aa9a13a727ec43c0a6bc01c7fd34ec72d1cddd
Partha Modak <44617799+parthamodak@users.noreply.github.com>
2026-03-04 12:21:32 +0000

    base case now is 1

--- Recursion.py
+++ Recursion.py
@@ -1,20 +1,17 @@
 def open_doll(size):
-    print("Opening the doll =", size)      # (1)
+    print("Total Opening the doll =", size)
 
-    if size == 0:                                    # (2)
-        print("Base case reached")                   # (3)
-        return                                      # (4)
+    if size == 1:
+        print("Base case reached")
+        print("closing doll", size)
+        return
 
-    print("Opening doll", size)                     # (5)
+    print("Opening doll", size)
 
-    open_doll(size - 1)                             # (6)
+    open_doll(size - 1)
 
-    print("Closing doll", size)                     
-
+    print("Closing doll", size)
 
 
 size = int(input("Enter the number of dolls: "))
-open_doll(size)                                          
-
-# here is a thing when this code exicute it allocate the same amount of
-#  the memory what it need to open then it for the loasing time it require same like open is 10 so ending will be 10 total 20 memory allocated
+open_doll(size)
